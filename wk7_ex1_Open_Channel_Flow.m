654dd5c2cfd78f83222f28b37a86e5d4e9f1c46d
Vicky <hikaru8889@gmail.com>
2022-10-07 09:36:57 +0700

    Update wk7_ex1_Open_Channel_Flow.m

--- wk7_ex1_Open_Channel_Flow.m
+++ wk7_ex1_Open_Channel_Flow.m
@@ -5,8 +5,8 @@ h = 0.01; h2 = h*h;
 z = 0:h:1; y = 0:h:1;
 [Z,Y] = meshgrid(z,y);
 nz = length(z); ny = length(y);
-
 U = zeros(ny, nz);
+
 for i = 1:10000
     for iz = 2:nz-1
         for iy = 2:ny-1
@@ -16,7 +16,6 @@ for i = 1:10000
     U(end,:) = U(end-1,:);
 end
 
-
-imagesc(z, y, U);
+imagesc(z, y, U); colorbar;
 set(gca, 'YDir', 'normal')
 xlabel('Z'); ylabel('Y'); axis image
